935893dec0b3f17f5dcb287c7b9a988ec5715aa5
meetmmpatel <meetmmpatel@gmail.com>
2018-12-17 15:48:50 -0500

    Added State level code

--- src/main/java/com/example/restdemocall/controller/UsStateController.java
+++ src/main/java/com/example/restdemocall/controller/UsStateController.java
@@ -2,6 +2,7 @@ package com.example.restdemocall.controller;
 
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,8 +16,8 @@ public class UsStateController {
 
     // HttpHeaders
     HttpHeaders headers = new HttpHeaders();
-
     String URL_US = "http://services.groupkt.com/state/get/USA/all";
+
 
     @GetMapping("usa/states")
     public String getAllStates(){
@@ -30,10 +31,30 @@ public class UsStateController {
         HttpEntity<String> entity = new HttpEntity<String>(headers);
 
         // Send request with GET method, and Headers.
-        ResponseEntity<String> response = restTemplate.exchange(URL_US,HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> response =
+                restTemplate.exchange(URL_US,HttpMethod.GET, entity, String.class);
 
         String result = response.getBody();
         return result;
 
     }
+
+    @GetMapping("usa/states/{abbr}")
+    public String getOneState(@PathVariable String abbr){
+        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+
+        // Request to return JSON format
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // HttpEntity<String>: To get result as String.
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        String URL_US_State = "http://services.groupkt.com/state/get/USA/" + abbr;
+        ResponseEntity<String> response =
+                restTemplate.exchange(URL_US_State,HttpMethod.GET, entity, String.class);
+        String result = response.getBody();
+        return result;
+
+    }
+
 }
